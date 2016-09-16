05eeb69ad90473ed4e68e27f386fdbb73c9d3bff
simonmaddox <simon@simonmaddox.com>
2016-09-16 12:18:03 +0100

    Initial Commit

---
+++ EncryptedSync/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EncryptedSync
+//
+//  Created by Simon Maddox on 16/09/2016.
+//  Copyright © 2016 Maddox Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EncryptedSync/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EncryptedSync
+//
+//  Created by Simon Maddox on 16/09/2016.
+//  Copyright © 2016 Maddox Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
